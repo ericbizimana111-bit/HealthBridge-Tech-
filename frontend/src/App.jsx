b743c160af9f39d48f10af979a3b579c29458d0f
Bizimana Eric <ericbizimana111@gmail.com>
2026-05-21 20:33:33 +0200

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Home from './pages/Home'
+function App() {
+  <div>
+  <Home />
+
+  </div>
+
+}
+export default App
---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react'
+
+export const Home = () => {
+
+    const [rotatingMessage, setRotatingMessage] = useSate(0);
+
+    const careMsgs = [
+        'Healing starts with conversation.',
+        'Your wellbeing matters.',
+        'Together we can overcome challenges',
+        'Support is always available',
+        'Prevention is better than cure',
+        'Share your problems to know how to overcome them',
+        'Better share ideas with your friend',
+        'You are not alone,we are here for you',
+        'Feel more comfortable and healthy.'
+    ];
+
+    useEffect(() => {
+
+        const interval = setInterval(() => {
+            setRotatingMessage((prev) => (prev + 1) % careMsgs.length);
+        }, 5000);
+        return () => clearInterval(interval);
+
+    }, []);
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
